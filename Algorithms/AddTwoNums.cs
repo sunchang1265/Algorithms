bf86843d0361eb0d7ad0454175d00aa23729f403
Chang <sunchang1265@gmail.com>
2021-02-28 00:27:46 -0500

    Clean up .sln

--- Algorithms/AddTwoNums.cs
+++ Algorithms/AddTwoNums.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Algorithms
+namespace Leetcode
 {
     public class AddTwoNums
     {
@@ -33,22 +33,23 @@ namespace Algorithms
                 else l2.val = temp;
                 return l2;
             }
-            while (l1.next != null)
-            {
-                int increment = 0;
-                int valResult = l1.val + l2.val;
-                if (valResult >= 10)
-                {
-                    result.val = valResult - 10;
-                    increment = 1;
-                }
-                l1Next = l1.next;
-                l2Next = l2.next;
-                if (l1.next == null || l2.next == null)
+            return result;
+            // while (l1.next != null)
+            // {
+            //     int increment = 0;
+            //     int valResult = l1.val + l2.val;
+            //     if (valResult >= 10)
+            //     {
+            //         result.val = valResult - 10;
+            //         increment = 1;
+            //     }
+            //     l1Next = l1.next;
+            //     l2Next = l2.next;
+            //     if (l1.next == null || l2.next == null)
 
-                if (l2.next == null)
+            //     if (l2.next == null)
 
-            }
+            // }
         }
     }
 
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSumSolution twoSum = new TwoSumSolution();
+            int[] input = new int[] { 2, 7, 11, 15 };
+            //int[] twoSumOutput = twoSum.TwoSum(input, 18);
+            int[] twoSumOutput = twoSum.TwoSumBetter(input, 18);
+            Console.WriteLine($"[{twoSumOutput[0]}], [{twoSumOutput[1]}]");
+        }
+    }
+}
